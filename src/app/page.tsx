c9ae3c9ad036b8c0b2e7d862d710e2fa5e15a2a9
Ahmed12590 <muhammadahmed12590@gmail.com>
2025-06-20 23:27:26 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold text-blue-300 mb-4">Welcome to the Employee Leave Portal</h1>
-      <p>This is the home page of the employee leave portal</p>
+      <p>This is the home page of the employee leave portal.</p>
     </div>
   );
 }
